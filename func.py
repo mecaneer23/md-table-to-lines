19363d76e8a4f7e2f9ea788b88db2abfa8bdac22
mecaneer23 <mecaneer23@gmail.com>
2023-05-13 10:49:12 -0500

    it finally works how I want it to work

--- func.py
+++ func.py
@@ -21,18 +21,46 @@ def md_table_to_lines(
         FileNotFoundError: If the specified markdown file cannot be found.
     """
 
+    # Check for valid line indices
     if last_line_idx <= first_line_idx:
         raise ValueError("Last line index must be greater than first line index.")
 
+    # Get raw lines from the markdown file
     try:
         with open(filename) as f:
             lines = f.readlines()[first_line_idx - 1 : last_line_idx - 1]
     except FileNotFoundError:
         raise FileNotFoundError("Markdown file not found.")
+    print(lines[0])
 
+    # Remove unwanted characters and split each line into a list of values
     for i, _ in enumerate(lines):
-        remove += ["|", "\n"]
         for item in remove:
-            lines[i] = lines[i].replace(item, "").strip()
-    lines[1] = lines[1].replace(" ", "-")
+            lines[i] = lines[i].replace(item, "")
+        lines[i] = lines[i].split("|")[1:-1]
+    column_count = len(lines[0])
+    lines[1] = ["-" for _ in range(column_count)]
+    print(lines[0])
+
+    # Create lists of columns
+    columns = [[0, []] for _ in range(column_count)]
+    for i in range(column_count):
+        for line in lines:
+            columns[i][1].append(line[i])
+    print(lines[0])
+
+    # Find the maximum length of each column
+    for i, (_, v) in enumerate(columns):
+        columns[i][0] = len(max([w.strip() for w in v], key=len))
+    lines[1] = ["-" * (l + 1) for l, _ in columns]
+    print(lines[0])
+
+    # Join the lines together into a list of formatted strings
+    for i, line in enumerate(lines):
+        for j, v in enumerate(line):
+            line[j] = v.strip().ljust(columns[j][0] + 2)
+        lines[i] = "".join(lines[i])
+    lines[1] = "-" * (
+        sum(columns[i][0] for i, _ in enumerate(columns)) + 2 * (len(columns) - 1)
+    )
     return lines
